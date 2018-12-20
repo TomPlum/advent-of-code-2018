19a31018cf97a7714de12164b87a20f7e7d6d7e0
TomPlum <Thomas.Plumpton@hotmail.co.uk>
2018-12-20 20:32:32 +0000

    Added Solution To Day 1 - Part 1

--- src/main/java/com/advent/day1/DeviceFrequencyCalculator.java
+++ src/main/java/com/advent/day1/DeviceFrequencyCalculator.java
@@ -4,15 +4,18 @@ import com.advent.common.PuzzleInputReader;
 
 import java.util.List;
 
-import static com.advent.common.PuzzleInputReader.readInput;
-
 public class DeviceFrequencyCalculator {
     public Integer calculateResultantFrequency() {
-        return 10;
+        List<String> puzzleInput =  PuzzleInputReader.readPuzzleInput(1);
+        return getFrequency(puzzleInput);
     }
 
     public Integer calculateResultantFrequency(String fileName) {
         List<String> puzzleInput =  PuzzleInputReader.readInput(fileName);
+        return getFrequency(puzzleInput);
+    }
+
+    private Integer getFrequency(List<String> puzzleInput) {
         Integer resultantFrequency = Integer.valueOf(puzzleInput.get(0));
         for (int i = 1; i < puzzleInput.size(); i++) {
             resultantFrequency += Integer.valueOf(puzzleInput.get(i));
--- src/test/java/com/advent/day1/Solution.java
+++ src/test/java/com/advent/day1/Solution.java
@@ -1,5 +1,9 @@
 package com.advent.day1;
 
 public class Solution {
-    
+    public static void main(String[] args) {
+        DeviceFrequencyCalculator calculator = new DeviceFrequencyCalculator();
+        Integer resultantFrequency = calculator.calculateResultantFrequency();
+        System.out.println(resultantFrequency);
+    }
 }
