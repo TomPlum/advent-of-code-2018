6ca39abe402278f8fd24b35fcb717b5fcf6b128b
TomPlum <Thomas.Plumpton@hotmail.co.uk>
2018-12-20 20:30:06 +0000

    Added DeviceFrequencyCalculator & Tested

--- src/main/java/com/advent/day1/DeviceFrequencyCalculator.java
+++ src/main/java/com/advent/day1/DeviceFrequencyCalculator.java
@@ -1,4 +1,22 @@
 package com.advent.day1;
 
+import com.advent.common.PuzzleInputReader;
+
+import java.util.List;
+
+import static com.advent.common.PuzzleInputReader.readInput;
+
 public class DeviceFrequencyCalculator {
+    public Integer calculateResultantFrequency() {
+        return 10;
+    }
+
+    public Integer calculateResultantFrequency(String fileName) {
+        List<String> puzzleInput =  PuzzleInputReader.readInput(fileName);
+        Integer resultantFrequency = Integer.valueOf(puzzleInput.get(0));
+        for (int i = 1; i < puzzleInput.size(); i++) {
+            resultantFrequency += Integer.valueOf(puzzleInput.get(i));
+        }
+        return resultantFrequency;
+    }
 }
--- src/test/java/com/advent/day1/DeviceFrequencyCalculatorTest.java
+++ src/test/java/com/advent/day1/DeviceFrequencyCalculatorTest.java
@@ -2,9 +2,29 @@ package com.advent.day1;
 
 import org.junit.jupiter.api.Test;
 
-class DeviceFrequencyCalculatorTest {
-    @Test
-    void calculateResultantFrequency() {
+import static org.assertj.core.api.Assertions.assertThat;
 
+class DeviceFrequencyCalculatorTest {
+    private final DeviceFrequencyCalculator deviceFrequencyCalculator = new DeviceFrequencyCalculator();
+
+    @Test
+    void calculateResultantFrequency_exampleInput1() {
+        String fileName = "example-input-1.txt";
+        Integer resultantFrequency = deviceFrequencyCalculator.calculateResultantFrequency(fileName);
+        assertThat(resultantFrequency).isEqualTo(3);
+    }
+
+    @Test
+    void calculateResultantFrequency_exampleInput2() {
+        String fileName = "example-input-2.txt";
+        Integer resultantFrequency = deviceFrequencyCalculator.calculateResultantFrequency(fileName);
+        assertThat(resultantFrequency).isEqualTo(0);
+    }
+
+    @Test
+    void calculateResultantFrequency_exampleInput3() {
+        String fileName = "example-input-3.txt";
+        Integer resultantFrequency = deviceFrequencyCalculator.calculateResultantFrequency(fileName);
+        assertThat(resultantFrequency).isEqualTo(-6);
     }
 }
