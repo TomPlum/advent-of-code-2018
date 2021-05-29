4666f8443237612aada64dedb9fae6f664415e8b
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-05-29 21:47:50 +0100

    Setup | Nuked Existing Gradle Configuration & Completed Java Days

--- src/main/java/com/advent/domain/Frequency.java
+++
@@ -1,9 +0,0 @@
-package com.advent.domain;
-
-import com.omega.library.domain.SingleValueObject;
-
-public class Frequency extends SingleValueObject<Integer> {
-    public Frequency(Integer value) {
-        super(value);
-    }
-}
