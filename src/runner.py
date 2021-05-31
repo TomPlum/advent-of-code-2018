1020bfdb5fa9cea89607bb5f20b1191bdbcde7e9
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-05-31 12:30:17 +0100

    Solutions | Added Rudimentary Benchmarking

--- src/runner.py
+++ src/runner.py
@@ -1,13 +1,27 @@
+import time
+
 from day1 import solution_part_1, solution_part_2
 
 
 def execute():
-    run(1, 1, solution_part_1)
-    run(1, 2, solution_part_2)
+    run(1, solution_part_1, solution_part_2)
 
 
-def run(day: int, part: int, func):
-    print(f"Day {day} [P{part}]: {func()}")
+def run(day: int, part1, part2):
+    print(f"[Day {day}]")
+    p1_result = measure(part1)
+    print(f"Part 1: {p1_result[0]}")
+    print(f"Execution Time: {p1_result[1]}s\n")
+    p2_result = measure(part2)
+    print(f"Part 2: {p2_result[0]}")
+    print(f"Execution Time: {p2_result[1]}s\n")
+    return p1_result[1], p2_result[1]
+
+
+def measure(func):
+    start = time.time()
+    answer = func()
+    return answer, time.time() - start
 
 
 if __name__ == '__main__':
