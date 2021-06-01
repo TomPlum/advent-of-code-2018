0246918e22c99d84bd1241bf6d98eb79c5766c44
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-06-01 19:59:54 +0100

    Day 2 | Cleaned Comments & Updated README Answer Table

--- src/day2.py
+++ src/day2.py
@@ -12,21 +12,11 @@ def part1(ids: [str]) -> int:
 def part2(ids: [str]) -> str:
     for a in ids:
         for b in ids:
-            print(f"comparing {a} with {b}")
             diff = list(difflib.ndiff(a, b))
-            print(f"diff {diff}")
-
             remaining = list(filter(lambda it: it[0] == ' ', diff))
-            print(f"remaining {remaining}")
-
             deltas = list(map(lambda it: it[0], diff))
-            print(f"deltas {deltas}")
-
             if deltas.count('+') == 1 and deltas.count('-') == 1:
-                lol = list(map(lambda it: it.strip(), remaining))
-                print(lol)
-                return ''.join(lol)
-                #return ''.join(set(a).intersection(b))
+                return ''.join(list(map(lambda it: it.strip(), remaining)))
 
 
 def occurrences(string: str, goal: int) -> bool:
@@ -41,4 +31,4 @@ def solution_part_1() -> int:
 
 
 def solution_part_2() -> str:
-    return part2(read(2).toString()) #Not abcdefghijklmnoprstuvxz
+    return part2(read(2).toString())
