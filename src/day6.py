015cae3c0d17b56053ed53592c9cd203d386345d
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-06-14 19:46:36 +0100

    Day 6 | Part 1 Progress

--- src/day6.py
+++ src/day6.py
@@ -3,6 +3,14 @@ from utility import Point2D
 
 def part1(data: [str]) -> int:
     coords = map(lambda s: Point2D(s.split(",")[0].strip(), s.split(",")[1].strip()), data)
+    x_max = max(pos.x for pos in coords)
+    y_max = max(pos.y for pos in coords)
+
+    all_coords = {}
+
+    for y in range(0, y_max):
+        for x in range(0, x_max):
+            all_coords[Point2D(x, y)] = "."
     for pos in coords:
         print(pos)
     return 0
